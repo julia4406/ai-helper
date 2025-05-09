d9e424905a9e224ae655a985fee14477eb489a6e
Julia Alexeeva <julia4406@gmail.com>
2025-05-09 10:30:23 +0300

    Add user schemas

--- src/api/schemas/user.py
+++ src/api/schemas/user.py
@@ -1,5 +1,29 @@
+from typing import Annotated
+from uuid import UUID
+from pydantic import BaseModel, Field
+
+
 class UserCreateSchema(BaseModel):
-  full_name: str
+  fullname: str
   username: str
   password: str
+
+
+class UserCreateResponseSchema(BaseModel):
+  id: UUID
   
+
+class UserUpdateSchema(BaseModel):
+  fullname: Annotated[str | None, Field()] = None
+  username: Annotated[str | None, Field()] = None
+  password: Annotated[str | None, Field()] = None
+
+
+class UserDetailResponseSchema(BaseModel):
+  id: UUID
+  fullname: str
+  username: str
+
+
+class UserListResponseSchema(BaseModel):
+  users: list[UserDetailResponseSchema]
