35b42d31acc36bcc056aa34f5f3f2730c96e8736
Julia Alexeeva <julia4406@gmail.com>
2025-05-09 10:41:05 +0300

    Add user service

--- src/services/user.py
+++ src/services/user.py
@@ -1,7 +1,11 @@
+from src.repositories.user import UserRepository
+from src.api.schemas.user import UserCreateSchema
+
+
 class UserService:
 
-  def __init__(self):
-    self._user_repo = ...
+  def __init__(self, user_repo: UserRepository):
+    self._user_repo = user_repo
 
   async def create_new_user(self, user: UserCreateSchema):
-    await self._user_repo
+    await self._user_repo.create_new_user(user=user)
