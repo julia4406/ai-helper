a15194112381dc31137ad991269ba7fc1c7c5b90
Julia Alexeeva <julia4406@gmail.com>
2025-05-10 21:23:49 +0300

    Add created_at field to all models

--- src/database/models/base.py
+++ src/database/models/base.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timezone
 from uuid import UUID, uuid4
 
+from sqlalchemy import DateTime, func
 from sqlalchemy.orm import declarative_base, Mapped, mapped_column
 
 Base = declarative_base()
 
 class IdModelMixin:
     id: Mapped[UUID] = mapped_column(primary_key=True, default=uuid4)
+    created_at: Mapped[datetime] = mapped_column(
+      DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
