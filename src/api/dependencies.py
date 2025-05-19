b28731fda6c8a172325a564fa29dcb353a472e09
Julia Alexeeva <julia4406@gmail.com>
2025-05-19 15:22:27 +0300

    Add dependency for question repository

--- src/api/dependencies.py
+++ src/api/dependencies.py
@@ -5,6 +5,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.clients.gemini.client import GeminiClientDep
 from src.repositories.interview import InterviewRepository
+from src.repositories.question import QuestionRepository
 from src.repositories.user_profile import ProfileRepository
 from src.services.interview.interview import InterviewService
 from src.services.user import UserService
@@ -45,16 +46,24 @@ def get_interview_repository(session: AsyncSessionDep) -> InterviewRepository:
 InterviewRepoDep = Annotated[InterviewRepository, Depends(get_interview_repository)]
 
 
+def get_question_repository(session: AsyncSessionDep) -> QuestionRepository:
+  return QuestionRepository(session=session)
+
+QuestionRepoDep = Annotated[QuestionRepository, Depends(get_question_repository)]
+
+
 def get_interview_service(
         interview_repo: InterviewRepoDep,
         user_repo: UserRepoDep,
         profile_repo: ProfileRepoDep,
+        question_repo: QuestionRepoDep,
         llm_client: GeminiClientDep
 ) -> InterviewService:
   return InterviewService(
     interview_repo=interview_repo,
     user_repo=user_repo,
     profile_repo=profile_repo,
+    question_repo=question_repo,
     llm_client=llm_client
   )
 
